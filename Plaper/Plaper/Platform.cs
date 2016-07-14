4bbf9c5411490f238ef6616b2e28a077f66952bd
lorencalebbaum <lorencalebbaum@cedarville.edu>
2016-07-14 19:42:33 -0400

    Added Platform class

---
+++ Plaper/Plaper/Platform.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;			//ADDED 7/14/2015 19:39
+using Microsoft.Xna.Framework.Graphics; //ADDED 7/14/2015 19:39
+using Microsoft.Xna.Framework.Input;    //ADDED 7/14/2015 19:39
+using System;                           //ADDED 7/14/2015 19:39
+using System.Collections.Generic;       //ADDED 7/14/2015 19:39
+using System.Linq;                      //ADDED 7/14/2015 19:39
+using System.Text;                      //ADDED 7/14/2015 19:39
+using System.Threading.Tasks;           //ADDED 7/14/2015 19:39
+
+namespace Plaper {                      //ADDED 7/14/2015 19:39
+	class Platform {
+
+	}
+}
