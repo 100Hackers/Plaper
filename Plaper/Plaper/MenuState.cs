636aeb26a288eef98f747eefe1c210d055085dfc
Spencer Graffunder <spencergraffunder@cedarville.edu>
2016-07-16 00:05:25 -0500

    added comments to menustate

--- Plaper/Plaper/MenuState.cs
+++ Plaper/Plaper/MenuState.cs
@@ -13,21 +13,26 @@ namespace Plaper {
     class MenuState : State {
 
         Player player;
+        GraphicsDeviceManager graphics;
+
         Rectangle settingsButtonPosition;
         Texture2D menuButtonTexture;
         Rectangle startButtonPosition;
-        GraphicsDeviceManager graphics;
 
         //ctor
         public MenuState(Player player, GraphicsDeviceManager graphics) {
             this.player = player;
             this.graphics = graphics;
+
+            //stuff for button texture
             menuButtonTexture = new Texture2D(graphics.GraphicsDevice, 20, 10);
             Color[] startTextureData = new Color[20*10];
             for (int i = 0; i < startTextureData.Length; i++) {
                 startTextureData[i] = Color.Chocolate;
             }
             menuButtonTexture.SetData(startTextureData);
+
+            //setting where the buttons will be
             settingsButtonPosition = new Rectangle(0, Game1.SCREEN_HEIGHT-(Game1.SCREEN_HEIGHT/10)-Game1.SCREEN_HEIGHT/5, Game1.SCREEN_WIDTH, Game1.SCREEN_HEIGHT/5);
             startButtonPosition = new Rectangle(0, settingsButtonPosition.Y-settingsButtonPosition.Height-(Game1.SCREEN_HEIGHT/10), Game1.SCREEN_WIDTH, Game1.SCREEN_HEIGHT/5);
         }
@@ -35,9 +40,9 @@ namespace Plaper {
         //update for game logic
         public override void Update(GameTime gameTime) {
 
+            //check where mouse is and do stuff if it's clicked
             var mouseState = Mouse.GetState();
             var mousePosition = new Point(mouseState.X, mouseState.Y);
-
             if(mouseState.LeftButton == ButtonState.Pressed) {
                 if(startButtonPosition.Contains(mousePosition)) {
                     State.setState(new GameState(player, graphics));
@@ -50,8 +55,10 @@ namespace Plaper {
 
             spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullCounterClockwise);
 
+            //start button
             spriteBatch.Draw(menuButtonTexture, startButtonPosition, Color.White);
 
+            //settings button
             spriteBatch.Draw(menuButtonTexture, settingsButtonPosition, Color.White);
 
             spriteBatch.End();
