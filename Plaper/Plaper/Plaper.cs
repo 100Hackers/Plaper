c9ce3229903b245a9bb25bcc9d837fdbf88532e0
bSkwared <gh@truebearsfan.com>
2016-07-28 01:23:24 -0400

    smaller player and platforms

--- Plaper/Plaper/Plaper.cs
+++ Plaper/Plaper/Plaper.cs
@@ -26,8 +26,8 @@ namespace Plaper {
         public const double PLAT_RATIO   = 50.0 / 250.0;
 
         // Sprite scales
-        public const double PLAYER_SCALE  = 0.07;
-        public const double PLAT_SCALE    = 0.17;
+        public const double PLAYER_SCALE  = 0.06;
+        public const double PLAT_SCALE    = 0.13;
         public const double BUTTON_HEIGHT = 0.1;
         public const double START_HEIGHT  = 0.09;
 
